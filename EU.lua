63ceb6145976a840c6cdf536ec99eef28ba60739
Jaydenn <63414528+TheNuggetEater05@users.noreply.github.com>
2024-02-17 21:37:01 -0500

    fix extensions

--- EU.lua
+++ EU.lua
@@ -1,56 +1,54 @@
 local EasyUtils = {}
 EasyUtils.Drawings = {}
 
-EasyUtils.Functions = {} do
-    function EasyUtils.Functions:dumpgc(path: string, filtertbl: table)
-        assert(path, 'missing_arg = path')
+function EasyUtils:dumpgc(path: string, filtertbl: table)
+    assert(path, 'missing_arg = path')
 
-        local filters = filtertbl or {}
-        for index, garbage in next, getgc() do
-            if not table.find(filters, tostring(typeof(garbage))) then
-                if typeof(garbage) == "Instance" then
-                    if not table.find(filters, tostring(garbage.ClassName)) then
-                        appendfile(tostring(path), '[IDX] = '..tostring(index)..' | [Item]: '..tostring(garbage)..' ~ [CType]: \''..typeof(garbage)..' = [RType]: \''..garbage.ClassName..'\'\n')
-                    end
-                elseif typeof(garbage) == "function" then
-                    appendfile(tostring(path), '[IDX] = '..tostring(index)..' | [Item]: '..tostring(garbage)..' ~ [Name]: '..tostring(debug.getinfo(garbage).name)..' ~ [CType]: \''..typeof(garbage)..'\'\n')
-                else
-                    appendfile(tostring(path), '[IDX] = '..tostring(index)..' | [Item]: '..tostring(garbage)..' ~ [CType]: \''..typeof(garbage)..'\'\n')
+    local filters = filtertbl or {}
+    for index, garbage in next, getgc() do
+        if not table.find(filters, tostring(typeof(garbage))) then
+            if typeof(garbage) == "Instance" then
+                if not table.find(filters, tostring(garbage.ClassName)) then
+                    appendfile(tostring(path), '[IDX] = '..tostring(index)..' | [Item]: '..tostring(garbage)..' ~ [CType]: \''..typeof(garbage)..' = [RType]: \''..garbage.ClassName..'\'\n')
                 end
+            elseif typeof(garbage) == "function" then
+                appendfile(tostring(path), '[IDX] = '..tostring(index)..' | [Item]: '..tostring(garbage)..' ~ [Name]: '..tostring(debug.getinfo(garbage).name)..' ~ [CType]: \''..typeof(garbage)..'\'\n')
+            else
+                appendfile(tostring(path), '[IDX] = '..tostring(index)..' | [Item]: '..tostring(garbage)..' ~ [CType]: \''..typeof(garbage)..'\'\n')
             end
         end
     end
+end
 
 
 
 
 
-    -- Drawing funcs
-    function EasyUtils.Functions:newdraw(type: string, props: table, where: table)
-        assert(type, 'missing_arg: type')
-        assert(props, 'missing_arg: props')
-        local proptbl = props
-        local whereidx = where or EasyUtils.Drawings
+-- Drawing funcs
+function EasyUtils:newdraw(type: string, props: table, where: table)
+    assert(type, 'missing_arg: type')
+    assert(props, 'missing_arg: props')
+    local proptbl = props
+    local whereidx = where or EasyUtils.Drawings
 
-        local draw = Drawing.new(type)
-        for prop, val in pairs(proptbl) do
-            draw[prop] = val
-        end
-
-        table.insert(whereidx, draw)
-        return draw
+    local draw = Drawing.new(type)
+    for prop, val in pairs(proptbl) do
+        draw[prop] = val
     end
 
-    function EasyUtils.Functions:editdraw(drawing, props: table)
-        assert(drawing, 'missing_arg: drawing')
-        assert(props, 'missing_arg: props')
-        local proptbl = props
+    table.insert(whereidx, draw)
+    return draw
+end
 
-        for prop, val in pairs(proptbl) do
-            drawing[prop] = val
-        end
-        return drawing
+function EasyUtils:editdraw(drawing, props: table)
+    assert(drawing, 'missing_arg: drawing')
+    assert(props, 'missing_arg: props')
+    local proptbl = props
+
+    for prop, val in pairs(proptbl) do
+        drawing[prop] = val
     end
+    return drawing
 end
 
 return EasyUtils
